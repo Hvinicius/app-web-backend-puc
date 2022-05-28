d8bcbf1f97f063730b59d3a279e851d628adcb13
Hvinicius <hvinicius@outlook.com>
2022-05-28 16:40:24 -0400

    feat: Add o model Veiculo.

---
+++ app-web-backend/Models/Veiculo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace app_web_backend.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Obrigatório Informar o nome!")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Obrigatório Informar aplaca!")]
+        public string Placa { get; set; }
+    }
+}
