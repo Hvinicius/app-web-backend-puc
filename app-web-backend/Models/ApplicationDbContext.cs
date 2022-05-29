cec11c6c3ffdc143006791046e4a859d99f5e314
Hvinicius <hvinicius@outlook.com>
2022-05-28 22:05:49 -0400

    feat: Config do Entity Framework

---
+++ app-web-backend/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace app_web_backend.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+      public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+      {
+      }
+
+        public DbSet<Veiculo>Veiculos { get; set; }
+    }
+}
